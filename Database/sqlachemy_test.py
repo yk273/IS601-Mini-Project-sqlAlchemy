08b23a182c6fe2cfa436b69c9fe861f3fad1a0d4
Yana Kokit <yk273@njit.edu>
2019-12-19 13:30:11 -0500

    Feature: Inserting Data-Customer

--- Database/sqlachemy_test.py
+++ Database/sqlachemy_test.py
@@ -16,3 +16,100 @@ Base = declarative_base()
 # this instantiates the session object
 session = Session()
 
+# Setting up classes (creates the record objects and defines the schema)
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    # Here we define columns for the table customer
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    address = Column(String(250), nullable=False)
+    town = Column(String(250), nullable=False)
+
+
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Insert a Customer in the customer table
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden')
+
+# Insert a Customer in the customer table
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+c1, c2
+
+# c1.first_name, c1.last_name
+# c2.first_name, c2.last_name
+
+# add the information from c1 and c2 to the Customer table
+session.add(c1)
+session.add(c2)
+
+c1.id, c2.id
+
+# commit the changes made (the added c1 and c2)
+session.commit()
+
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
+
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
