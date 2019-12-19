496193ceea549852e41b0a53b34b494023c85d2d
Yana Kokit <yk273@njit.edu>
2019-12-19 16:02:33 -0500

    Feature: Casting

--- Database/sqlachemy_test.py
+++ Database/sqlachemy_test.py
@@ -339,3 +339,20 @@ session.query(
     func.count(distinct(Customer.town)),
     func.count(Customer.town)
 ).all()
+
+'''
+CASTING
+'''
+'''
+from sqlalchemy import cast, Date, distinct, union
+
+session.query(
+    cast(func.pi(), Integer),
+    cast(func.pi(), Numeric(10, 2)),
+    cast("2010-12-01", DateTime),
+    cast("2010-12-01", Date),
+).all()
+'''
+
+
+
