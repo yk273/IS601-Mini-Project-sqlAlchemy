afaa27bfd819132c6790f8244ebaccb3ff3f200f
Yana Kokit <yk273@njit.edu>
2019-12-19 16:01:01 -0500

    Feature: Dealing with Duplicates

--- Database/sqlachemy_test.py
+++ Database/sqlachemy_test.py
@@ -327,4 +327,15 @@ session.query(
     Customer.town
 ).group_by(Customer.town).having(func.count("*") > 2).all()
 
+'''
+INSERTING DATA
+'''
+from sqlalchemy import distinct
 
+session.query(Customer.town).filter(Customer.id < 10).all()
+session.query(Customer.town).filter(Customer.id < 10).distinct().all()
+
+session.query(
+    func.count(distinct(Customer.town)),
+    func.count(Customer.town)
+).all()
