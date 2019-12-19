9b8492606ffe80edfbd11f3ad0c643b954398e9e
Yana Kokit <yk273@njit.edu>
2019-12-19 13:30:50 -0500

    Feature: Inserting Data-Item

--- Database/sqlachemy_test.py
+++ Database/sqlachemy_test.py
@@ -29,7 +29,14 @@ class Customer(Base):
     address = Column(String(250), nullable=False)
     town = Column(String(250), nullable=False)
 
-
+class Item(Base):
+    __tablename__ = 'item'
+    # Here we define columns for the table customer
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    cost_price = Column(Integer)
+    selling_price = Column(Integer)
+    quantity = Column(Integer)
 
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
@@ -112,4 +119,16 @@ c6 = Customer(first_name='Scott',
               )
 
 session.add_all([c3, c4, c5, c6])
+session.commit()
+
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
 session.commit()
